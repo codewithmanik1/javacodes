12d5dacd581ca26c09965900ee0fa17f5a253047
Admin <admin@Admins-MacBook-Pro.local>
2025-05-08 21:57:56 +0530

    Unary Operator

---
+++ src/operators/unaryoperator/UnaryOperator.java
@@ -0,0 +1,15 @@
+package operators.unaryoperator;
+
+public class UnaryOperator {
+
+    public static void main(String[] args) {
+
+        // ++ : increment
+        // -- : decrement
+
+        int a = 10;
+        //post increment
+        System.out.println(a++);
+        System.out.println(a);
+    }
+}

2550ad9bb240fc6993302d5b9146becd4a9f071d
Admin <admin@192.168.1.6>
2025-05-13 21:54:41 +0530

    Ternary Operator

--- src/Basics/ProgramOne.java
+++ src/Basics/ProgramOne.java
@@ -2,15 +2,6 @@ package Basics;
 
 public class ProgramOne {
     public static void main(String[] args) {
-
-        int a = 66;
-        int b = a + 'A';
-
-        System.out.println(a);          //66
-        System.out.println(a + b);      //66 + 66 + 65
-
-        a = 60;
-        int c = 69;
-        System.out.println(c);    //69
+        System.out.println("hello world");
     }
 }
--- src/operators/ternaryoperator/TernaryOperator.java
+++ src/operators/ternaryoperator/TernaryOperator.java
@@ -1,4 +1,27 @@
 package operators.ternaryoperator;
 
 public class TernaryOperator {
+    public static void main(String[] args) {
+
+        //ternary operator
+        //condition ? true : false
+        //condition ? "true executes" : "false executes"
+
+        int nMarks = 72;                            //nMarks - 72
+        int dMarks = 94;                            //dMarks - 94
+
+        //ternary operator
+        // "" - In double quotes string is store
+
+        //condition ? "if condition is true then this block" : "if condition false then this block"
+
+        String ans = (nMarks > dMarks) ? "Niraj has a lower score" : "Deepali has a higher score than Niraj";
+        //ans -
+        //(nMarks > dMarks) ? "Niraj have less score" : "Deepali have high score than neeraj";
+        // 72 > 94 ?  "Niraj have less score" : "Deepali have high score than neeraj";
+        System.out.println(ans);
+
+        int myAns = (nMarks <= dMarks) ? 100 : 101;
+        System.out.println(myAns);
+    }
 }
